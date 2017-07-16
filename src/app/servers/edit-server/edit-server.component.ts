dc4cfa2b58190a0f2868730ca113758bcd22210a
Maksim <teplyy.maksim@gmail.com>
2017-07-17 00:29:42 +0300

    First/Last commit

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { ServersService } from '../servers.service';
+
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
+  server: {id: number, name: string, status: string};
+  serverName: string = '';
+  serverStatus: string = '';
+  allowEdit: boolean = false;
+  changesSaved = false;
+
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+
+    // NOTE: To make reactive use code below instead snapshot
+    this.route.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      );
+    // this.route.fragment.subscribe();
+
+    const id = parseInt(this.route.snapshot.params['id']);
+    this.server = this.serversService.getServer(id);
+
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Discard the changes?')
+    } else {
+      return true;
+    }
+  }
+}
